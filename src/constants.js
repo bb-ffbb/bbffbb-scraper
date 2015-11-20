ace03be1fce442a6ab3db651a1018406940796ba
Luca De Feo <dfl@deagol>
2015-11-20 03:19:18 +0100

    Some (incomplete) metadata on the mobiles.ffbb.com API

--- src/constants.js
+++ src/constants.js
@@ -30,3 +30,102 @@ exports.licenseFields = {
   qualificationDate: 'Date qualif.',
   licenseType: 'Type lic.'
 };
+
+exports.mobileAPI = {
+  version: '1.0.3',
+  baseURL: 'http://mobiles.ffbb.com/php/v1_0_3/',
+  endpoints: {
+    championships: {
+      url: 'topChampionships.php',
+      type: 'POST',
+      desc: 'List of championships organised by FFBB',
+      parameters: [
+        {
+          name: 'type',
+          values: ['championship', 'cup'],
+          required: true
+        }
+      ],
+      fields: [ 'name', 'id', 'type' ]
+    },
+    departements: {
+      url: 'areas.php',
+      type: 'POST',
+      desc: 'List of "departements" managed by FFBB',
+      parameters: [
+        {
+          name: 'type',
+          values: ['championship', 'cup'],
+          required: true
+        }
+      ],
+      fields: [ 'name', 'id' ]
+    },
+    deptChampionships: {
+      url: 'areaCompetitions.php',
+      type: 'POST',
+      desc: 'List of championships by "departement"',
+      parameters: [
+        {
+          name: 'type',
+          values: [ 'championship', 'cup' ],
+          required: true
+        },
+        {
+          name: 'id',
+          required: true
+        }
+      ],
+      fields: [ 'name', 'id', 'groupField' ]
+    },
+    results: {
+      url: 'results.php',
+      type: 'POST',
+      desc: 'Results by championship',
+      parameters: [
+        {
+          name: 'type',
+          values: ['championship', 'cup'],
+          required: true
+        },
+        {
+          name: 'id',
+          required: true
+        },
+        {
+          name: 'group',
+          values: [ 'Masculin', 'F\u00e9minin' ]
+        },
+        {
+          name: 'subCompetition',
+        }
+      ],
+      fields: {
+        'currentDay': null,
+        'days': [ 'desc', 'name' ],
+        'groups': [ 'id', 'name', 'type' ],
+        'matchs': [ 'date', 'formattedDate', 'hometeam',
+                    'matchId', 'score', 'time', 'visitorteam' ],
+        'standings': [ 'arb', 'club', 'conceded', 'day', 'defaults',
+                       'draw', 'ent', 'forfeited', 'initi', 'lost',
+                       'penalties', 'points', 'pos', 'quotient',
+                       'scored', 'win' ],
+        'subCompetitions': [ 'id', 'name']
+      }
+    },
+    lives: {
+      url: 'lives.php',
+      type: 'GET',
+      desc: 'Live games',
+      fields: {
+        'category' : null,
+        'matchs': [ 'awayscore', 'awayteam', 'date', 'datetime', 'homescore',
+                    'hometeam', 'link', 'running', 'time', 'upcoming' ],
+        'title': null
+    },
+    search: {
+      url: 'search.php',
+      desc: 'Search tool'
+    }
+  }
+};
